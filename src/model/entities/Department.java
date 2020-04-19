15e7c520a471bdb2352e40ab5b77ec755dbaec0a
Renan Klinsmann <klismann-nan@hotmail.com>
2020-04-19 14:21:24 -0300

    Adding database access

--- src/model/entities/Department.java
+++ src/model/entities/Department.java
@@ -5,10 +5,10 @@ import java.io.Serializable;
 public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
-
+	
 	private Integer id;
 	private String name;
-
+	
 	public Department() {
 	}
 
@@ -39,7 +39,6 @@ public class Department implements Serializable {
 		int result = 1;
 		result = prime * result + ((id == null) ? 0 : id.hashCode());
 		return result;
-
 	}
 
 	@Override
@@ -50,23 +49,18 @@ public class Department implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		
 		Department other = (Department) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
-		} 
-		else if (!id.equals(other.id))
-
+		} else if (!id.equals(other.id))
 			return false;
-
 		return true;
-
 	}
 
 	@Override
 	public String toString() {
 		return "Department [id=" + id + ", name=" + name + "]";
 	}
-
+	
 }
